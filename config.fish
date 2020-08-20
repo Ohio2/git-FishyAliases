5eba51f045d437f5b9794d24627c1a22fb236f48
Ohio2 <61843348+Ohio2@users.noreply.github.com>
2020-08-20 07:59:04 +0200

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,6 @@
+# This script was made by 
+# Ohio2!
+# Thanks for using this
+# Requires PFETCH!!!
+set -g -x fish_greeting 'Remember that fish config aka (something in the sort of) .fishrc is located in .config/fish/functions/fish_prompt.fish!'
+pfetch
